7ad891e3f79c58bfc00c3cff68e18ac59118ff6e
sarahOmar <117687177+codeQueen211@users.noreply.github.com>
2023-06-22 17:18:01 +0300

    Add files via upload

---
+++ project1.js
@@ -0,0 +1,29 @@
+const personal=document.querySelector('.personal')
+personal.addEventListener('click', function(){
+    window.open('file:///C:/Users/sarao/personal.html', '_blank');
+});
+
+const education=document.querySelector('.education')
+education.addEventListener('click', function(){
+    window.open('hhttps://www.pinterest.com/pin/563018690738092/', '_blank');
+    ;});
+
+const projects=document.querySelector('.projects')
+projects.addEventListener('click', function(){
+    window.open('hhttps://www.pinterest.com/pin/563018690738092/', '_blank');
+    });
+
+const workExperince=document.querySelector('.workExperince')
+workExperince.addEventListener('click', function(){
+    window.open('hhttps://www.pinterest.com/pin/563018690738092/', '_blank');
+});
+
+const languages=document.querySelector('.languages')
+languages.addEventListener('click', function(){
+    window.open('hhttps://www.pinterest.com/pin/563018690738092/', '_blank');
+});
+
+const contact=document.querySelector('.contact')
+contact.addEventListener('click', function(){
+    window.open('hhttps://www.pinterest.com/pin/563018690738092/', '_blank');
+});    
