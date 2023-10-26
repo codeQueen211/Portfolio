0cc7e83a4fea9226fc422d13ee053ad01279a187
sarahOmar <117687177+codeQueen211@users.noreply.github.com>
2023-10-26 23:43:02 +0300

    update contact page

--- project1.js
+++ project1.js
@@ -18,10 +18,13 @@ function openLanguagePage(){
          const newpageURL="languages.html";
          window.open(newpageURL, '_blank');
 }
+function openContactPage(){
+    const newpageURL="contact.html";
+    window.open(newpageURL, '_blank');
+}
 
 
-
-const contact=document.querySelector('.contact')
-contact.addEventListener('click', function(){
-    window.open('file:///C:/Users/sarao/OneDrive/%D7%A9%D7%95%D7%9C%D7%97%D7%9F%20%D7%94%D7%A2%D7%91%D7%95%D7%93%D7%94/project1/contact.html', '_blank');
-});    
+// const contact=document.querySelector('.contact')
+// contact.addEventListener('click', function(){
+//     window.open('file:///C:/Users/sarao/OneDrive/%D7%A9%D7%95%D7%9C%D7%97%D7%9F%20%D7%94%D7%A2%D7%91%D7%95%D7%93%D7%94/project1/contact.html', '_blank');
+// });    
